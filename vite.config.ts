a99905363cca94317ebf3f78a1e5e24222b98502
BrettOrmsby <brettwormsby@outlook.com>
2025-07-02 08:23:25 -0400

    build: configure dev setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
+  plugins: [
+    dts({
+      outDir: "dist",
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
-      entry: 'src/index.ts',
-      name: 'ShapeFall',
-      formats: ['es', 'cjs'],
-      fileName: (format) => `index.${format}.js`
+      entry: "src/index.ts",
+      name: "ShapeFall",
+      formats: ["es", "cjs"],
+      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ['matter-js'],
+      external: ["matter-js"],
     },
   },
 });
